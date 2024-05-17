0173e903541e9483af5a07ced6cd6a42abc5a022
Cássio Zanon Bindaco <116852683+Ca22io@users.noreply.github.com>
2024-05-17 17:10:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-if __name__ == "__main__":
-    uvicorn.run(app="main:app", reload=True)
+# if __name__ == "__main__":
+#     uvicorn.run(app="main:app", reload=True)
